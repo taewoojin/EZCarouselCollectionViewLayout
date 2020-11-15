b95cb02f45766babe1a06abf2f8a73585b3cbcbf
taewoo <>
2020-11-15 18:00:52 +0900

    Initial Project Setting

---
+++ CarouselCollectionViewLayout/CarouselCollectionViewLayout.h
@@ -0,0 +1,18 @@
+//
+//  CarouselCollectionViewLayout.h
+//  CarouselCollectionViewLayout
+//
+//  Created by 태우 on 2020/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarouselCollectionViewLayout.
+FOUNDATION_EXPORT double CarouselCollectionViewLayoutVersionNumber;
+
+//! Project version string for CarouselCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char CarouselCollectionViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselCollectionViewLayout/PublicHeader.h>
+
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 태우 on 2020/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
